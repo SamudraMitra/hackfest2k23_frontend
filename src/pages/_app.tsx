e95728ae02c7b0cac94f1a40b517f75b7d3fcc67
Samudra <samudramitra2017@gmail.com>
2023-06-03 11:32:07 +0530

    commited

---
+++ src/pages/_app.tsx
@@ -0,0 +1,48 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserProvider from '../contexts/user.context'
+import { SmoothProvider } from 'react-smooth-scrolling'
+
+function Loading() {
+  return (
+    <>
+      <div className="spinner-wrapper">
+        {/* <div className="spinner" /> */}
+        <img src='./assets/images/gallery/hackfest_logo.gif' className='spinner'/>
+      </div>
+    </>
+  )
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
+  })
+  useEffect(() => {
+    const threeScript = document.createElement('script')
+    threeScript.setAttribute('id', 'threeScript')
+    threeScript.setAttribute(
+      'src',
+      'https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js',
+    )
+    document.getElementsByTagName('head')[0].appendChild(threeScript)
+    return () => {
+      if (threeScript) {
+        threeScript.remove()
+      }
+    }
+  }, [])
+
+  return (
+    // <SmoothProvider skew={true}>
+    <>{loading ? <Loading /> : <Component {...pageProps} />}</>
+    // </SmoothProvider>
+  )
+}
